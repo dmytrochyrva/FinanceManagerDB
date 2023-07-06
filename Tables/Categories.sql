cef97046b9313974771f36c97211de0501929c83
Dmytro Chyrva <dmytro.chyrva@protonmail.com>
2023-07-06 20:31:06 +0200

    [00000] Basic Tables Created.

---
+++ Tables/Categories.sql
@@ -0,0 +1,18 @@
+CREATE TABLE
+  CATEGORIES (ID INT PRIMARY KEY, NAME VARCHAR(36));
+
+INSERT INTO
+  CATEGORIES (NAME)
+VALUES
+  (1, 'Income'),
+  (2, 'Rent'),
+  (3, 'Utilities'),
+  (4, 'Groceries'),
+  (5, 'Savings'),
+  (6, 'Blow Money'),
+  (7, 'Lunch'),
+  (8, 'Transport'),
+  (9, 'Clothes'),
+  (10, 'Health'),
+  (11, 'Entertainment'),
+  (12, 'Hygiene');
